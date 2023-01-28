f4b37fb3aa51f02e5b1853d4f5f071ba7ccf2d75
Sergio <neftianik@inbox.ru>
2023-01-28 13:53:58 +0300

    Create ORM. Create products_images.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import ProductCategory, Poduct
+
+admin.site.register(Poduct)  #  registry
+admin.site.register(ProductCategory)
--- products/views.py
+++ products/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from products.models import ProductCategory, Poduct
+
 
 # Create your views here.
 # controllers (functions)
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
+
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store-Каталог',
+        'products': Poduct.objects.all(),
+        'categories': ProductCategory.objects.all(),
+        # [
+        #     {'image': '/static/vendor/img/products/Adidas-hoodie.png',
+        #      'name': 'Худи черного цвета с монограммами adidas Originals',
+        #      'price': 6090,
+        #      'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+        #      },
+        #     {'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+        #      'name': 'Синяя куртка The North Face',
+        #      'price': 2372,
+        #      'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+        #      },
+        #     {'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+        #      'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+        #      'price': 3390,
+        #      'description': 'Плотная ткань. Легкий материал.',
+        #      },
+        # ]
+    }
+    return render(request, 'products/products.html', context)
